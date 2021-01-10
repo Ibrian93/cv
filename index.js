62d720d08a1730a76b7f0d4f77714afa64a81e04
Brian Icochea <ibrian93@gmail.com>
2021-01-10 20:54:36 +0100

    Adding JQuery Fading

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("introducing js to my cv");
+//$("#title h1").remove();
+//$("#title h3").remove();
+
+//$("#title").text("Hello!");
+var titleFadeOut = false;
+
+$(document).ready(function() {
+    function complete() {
+        $("#title h1").text("Welcome to my Website").fadeIn(3000);
+    }
+    $("#title h1").hide().fadeIn(3000).fadeOut(3000, complete);
+});
+
